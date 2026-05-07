328a14302f8676bab2379d5e96c7d56e2bbf847a
pedrocode181-ship <pedrocode181@gmail.com>
2026-05-07 14:17:14 -0300

    Add files via upload

--- sellix/projeto/projeto/settings.py
+++ sellix/projeto/projeto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a=n=ux9fs!fnx&5te_abe)_=m0#yyz@mz1m-75-(_nn+*9vti^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
@@ -116,5 +116,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
